04cd9c89035389b891f68a6ff11254577028c6d0
guoshaoran <2043035530@qq.com>
2026-04-08 19:38:28 +0800

    fix：修改_init_

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,5 @@
-from .unet_model import UNet
+
+from .baseline_unet import BaselineUNet
+from .cbam_unet import CBAMUNet
+from .denseaspp_unet import DenseASPPUNet
+from .denseaspp_cbam_unet import DenseASPPCBAMUNet
